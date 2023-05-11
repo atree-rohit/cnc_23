0cae5419265c109c3d1030a6d6e1bfebc6af1a86
atree-rohit <rohit.george@atree.org>
2023-05-11 16:47:29 +0530

    mode switcher

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,63 @@
+<style>
+  .switcher{
+    display: flex;
+    justify-content: center;
+    margin: 0.5rem;
+  }
+  .switcher .btn{
+    font-size: 1.25rem;
+    padding: .5rem 1rem;
+  }
+
+  .switcher .btn.selected{
+    background-color:green;
+  }
+
+  .switcher .btn:first-child{
+    border-radius: 5rem 0 0 5rem;
+  }
+
+  .switcher .btn:last-child{
+    border-radius: 0 5rem 5rem 0;
+  }
+</style>
+
 <template>
-  <DistrictWiseStats />
+  <div class="switcher">
+    <button
+      class="btn"
+      v-for="m in modes"
+      :key="m"
+      :class="{'selected': m === mode}"
+      @click="mode = m"
+      v-text="m"
+    />
+  </div>
+  <Superlatives v-if="mode=='table'" />
+  <Map v-else />
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue'
   import ReadCSV from './components/ReadCSV.vue'
   import DistrictWiseStats from './components/DistrictWiseStats.vue'
+  import Superlatives from './components/Superlatives.vue'
+  import Map from './components/Map.vue'
   import store from './store'
 
   export default defineComponent({
     name: "App",
     components: {
       ReadCSV,
-      DistrictWiseStats
+      DistrictWiseStats,
+      Superlatives,
+      Map,
+    },
+    data(){
+      return {
+        modes: ['table', 'map'],
+        mode: 'map',
+      }
     },
     mounted(){
       store.dispatch('initLocationGroups')
